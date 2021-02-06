1ed579fda495bde9dafb8a028c7f517df9a5099e
“rahulitibd” <rahulitibd@gmail.com>
2021-02-06 11:27:05 +0600

    COde_update

--- 26.go
+++ 26.go
@@ -12,9 +12,29 @@ func main(){
 
 //		i++
 //	}
-i:=1
-for {
-	fmt.Println(i)
-	break
+//for {
+//	fmt.Println(i)
+//	break
+//}
+/*if 9%2==0{
+		fmt.Println("9 is even")
+	}else{
+		fmt.Println("9 is odd")
+	}*/
+	//if 8%2==0{
+		//fmt.Println("8 is odd")
+	//}else{
+		//fmt.Println("8 is even")
+	//}
+/*if num:=10; num>0{
+	fmt.Println(num, "is negative ")
+}else if num>10{
+	fmt.Println(num, " is posetive")
+	}
+*/
+if num:= 9; num<0{
+	fmt.Println(num, "is negative")
+}else{
+	fmt.Println(num, "is posetive")
 }
 }
